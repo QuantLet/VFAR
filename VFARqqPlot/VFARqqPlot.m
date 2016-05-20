c2188e905dd8c40f1860967e173fd9537e161da1
wschua88 <chuaweesong@hotmail.com>
2016-05-20 16:03:06 +0200

    Update VFARqqPlot.m

--- VFARqqPlot/VFARqqPlot.m
+++ VFARqqPlot/VFARqqPlot.m
@@ -2,7 +2,14 @@
 % load relevant data
 load('RES.mat')
 
+% standardize the residual
+stdres = zeros( size(RES) );
+for i=1:size(RES,1)
+    stdres(i,:) = ( RES(i,:) - mean( RES(i,:) ) ) / sqrt( var( RES(i,:) ) );
+end
+stdres = stdres(:);
+
 % plot
-qqplot(RES(:))
-title( 'QQ Plot of Residuals versus Standard Normal' );
+qqplot(stdres)
+title( 'QQ Plot of Residuals versus Standard Normal for AEZS' );
 ylabel( 'Quantiles of Residuals' )
